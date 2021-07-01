088f660c88baf13ba8ce4bcb270f356e0a3323c9
Cesar Poumian <cepoumian@Cesars-MacBook-Pro.local>
2021-06-30 22:47:28 -0600

    Create pages and components for home, blog, single post and about

---
+++ pages/blog/index.js
@@ -0,0 +1,3 @@
+import BlogPage, { getStaticProps } from './page/[page_index]'
+export { getStaticProps }
+export default BlogPage
--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -2,25 +2,20 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import Link from 'next/link'
-import Layout from '../components/Layout'
-import Post from '../components/Post'
-import { sortByDate } from '../utils'
+import Layout from '../../../components/Layout'
+import Post from '../../../components/Post'
+import { sortByDate } from '../../../utils'
 
-export default function HomePage({ posts }) {
+export default function BlogPage({ posts }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
       </div>
-      <Link href='/blog'>
-        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
-          All Posts
-        </a>
-      </Link>
     </Layout>
   )
 }
@@ -42,6 +37,6 @@ export async function getStaticProps() {
   })
 
   return {
-    props: { posts: posts.sort(sortByDate).slice(0, 6) },
+    props: { posts: posts.sort(sortByDate) },
   }
 }
